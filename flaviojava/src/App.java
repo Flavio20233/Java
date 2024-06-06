012002566be6cdc278d5c21dc0e1baeaee7e0f4b
FlavioB03 <145469124+Flavio20233@users.noreply.github.com>
2024-06-06 17:35:40 -0300

    Aulas tipos variaveis e condicao

---
+++ flaviojava/src/App.java
@@ -0,0 +1,41 @@
+public class App {
+    public static void main(String[] args)  {
+        System.out.println("Hello, World!");
+        
+
+        int primeiro = 4;
+        int segundo = 54;
+        double terceiro = 45;
+        double resultado;
+        resultado = primeiro *(segundo + terceiro);
+
+        System.out.println(resultado);
+        System.out.printf("%f\n",resultado);
+        String nome = "Flavio";
+        String sobreNome = "Barbosa";
+
+        System.out.printf(nome + " " + sobreNome);
+
+        int idade = 17;
+
+        if (idade>= 18) {
+            System.out.println("voce e maior de idade!");
+         }
+         System.out.println("voce e menor de idade!"); 
+
+         resultado = primeiro * segundo;
+         
+         if (resultado > 0)
+
+         System.out.printf(" %s,o numero %d positivo ");
+         else
+         system.out.printf("resultado negativo!");
+
+   
+ }
+}
+   
+
+
+
+
