0a8d10b42171610c0a7ee6336fb4db7097cd1fc2
FlavioB03 <flavio.barbosaoliveira@gmail.com>
2024-07-12 14:33:18 -0300

    api

---
+++ api/src/main/java/br/com/api/api/modelo/Pessoa.java
@@ -0,0 +1,36 @@
+package br.com.api.api.modelo;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "Populacao") // Caso queira mudar o nome da tabela, senão a tabela recebe o nome da classe
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(generator= "UUID")
+    private UUID id;
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
