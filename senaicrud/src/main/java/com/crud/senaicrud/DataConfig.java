c1e8692d4f963d024f57a260a5d7ed6c909abee2
FlavioB03 <flavio.barbosaoliveira@gmail.com>
2024-07-12 14:32:56 -0300

    senai spring

--- senaicrud/src/main/java/com/crud/senaicrud/DataConfig.java
+++ senaicrud/src/main/java/com/crud/senaicrud/DataConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
 
 @Configuration
 public class DataConfig {
@@ -15,23 +15,23 @@ public class DataConfig {
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.maysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/db_crudsenai");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mariadb://localhost:3306/db_crudsenai?user=root&password=root");
         dataSource.setUsername("root");
         dataSource.setPassword("root");
         return dataSource;
     }
-    @Bean 
+
+    @Bean
     public JpaVendorAdapter jpaVendorAdapter(){
         HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.MYSQL);
+        // adapter.setDatabase(Database.MARIADB);
         adapter.setShowSql(true);
         adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform("org.hibernate.dialect.MYSQL5Dialect");
+adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
         adapter.setPrepareConnection(true);
-        return adapter;
+        return adapter;        
 
     }
 
 }
-
--- senaicrud/src/main/java/com/crud/senaicrud/SenaicrudApplication.java
+++ senaicrud/src/main/java/com/crud/senaicrud/SenaicrudApplication.java
@@ -1,7 +1,5 @@
 package com.crud.senaicrud;
 
-import java.util.list;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
